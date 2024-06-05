6f9ff55007db150e2542de45f30eed68d79d49cf
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-06-05 09:49:08 +0530

    Day 1 - authController, authRouter, schema completed

---
+++ Routers/auhtRouter.js
@@ -0,0 +1,8 @@
+import express from express;
+import { registerUser } from '../Controllers/authController.js';
+
+const router = express.Router();
+
+router.post('/registe-user',registerUser)
+
+export default router;
